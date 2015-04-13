cf9bcfb32f09de95067049b5da2d98246fa9a85c
Palmer Dabbelt <palmer.dabbelt@eecs.berkeley.edu>
2015-04-12 21:53:38 -0700

    Add a prototype for an 8b/10b codec
    This itself doesn't do anything, but it does at least attempt to iron
    down the interface for the actual line protocal we're going to be
    using from now on.

---
+++ src/main/scala/protocol_8b10b.scala
@@ -0,0 +1,18 @@
+import chisel._
+
+package Serial {
+  // A single 8b/10b encoder/decoder, attached to the phy (for actual
+  // transmission) and to the rest of the chip (via the "controller"
+  // IO bundle).
+  class 8b10bController(channel_count: Int, phy_config_bundle: Bundle) extends Module {
+    class IO extends Bundle {
+      val phy = new PhyIO(encoded_word_bits = 10,
+                          channel_count = channel_count,
+                          config_bundle = phy_config_bundle)
+      val controller = new ControllerIO(8,
+                                        channel_count = channel_count,
+                                        phy_config_bundle = phy_config_bundle)
+    }
+    val io = new IO()
+  }
+}
